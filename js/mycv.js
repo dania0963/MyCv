c5eb1a6259c693ef89d00b76b4530367f659bd62
dania0963 <daniamdqt@gmail.com>
2022-09-07 20:44:34 +0300

    add my cv

---
+++ js/mycv.js
@@ -0,0 +1,10 @@
+function shadow(elem){
+elem.style.boxShadow="5px 5px 15px rgb(26, 148, 219) " ;
+}
+function noshadow(elem){
+    elem.style.boxShadow="none" ;
+    }
+function noside()
+{
+    document.getElementById("side").style.display="none";
+}    
